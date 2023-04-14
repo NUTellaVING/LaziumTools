a12f1332ad09b7c10e90625ed008a2d7ae93092d
CheapeeWastaken <cheapeewastaken@gmail.com>
2023-04-14 12:43:25 +0700

    Create PandaKey.lua

---
+++ PandaKey.lua
@@ -0,0 +1,7 @@
+local PandaKey = {}
+
+function PandaKey:name(text)
+	local pandacore = loadstring(game:HttpGet("https://pandatechnology.xyz/service_api/PandaAuthCore.lua", true))() -- Library of Key System
+	local service_name  = text -- Given by SkieHacker
+end
+return DiscordLib
